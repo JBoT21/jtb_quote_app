db5263a231880ef49057cf432788d1211f673c14
JBoT21 <bowen.jackson@yahoo.com>
2026-05-22 13:23:00 -0400

    Made quote app

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'QuoteCard.dart';
+
+void main () => runApp(MaterialApp(
+  home: quoteList(),
+
+    
+));
+
+class quoteList extends StatefulWidget {
+  const quoteList({super.key});
+
+  @override
+  State<quoteList> createState() => _quoteListState();
+}
+
+class _quoteListState extends State<quoteList> {
+
+  List<Quote> quotes = [
+    Quote(author:"Oscar Wild", text:"Be yourself, everyone else is already taken",
+    category: "Inspirational", createdAt: DateTime(1967, 1, 1)),
+    Quote(author:"Oscar Wild", text:"I have nothing to declare except my genius",
+    category: "biographical", createdAt: DateTime(1910, 1, 3)),
+    Quote(author:"Oscar Wild", text:"The truth is rarely pure and never simple",
+    category: "reflective", createdAt: DateTime(1896, 1, 1)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Quotes"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(quote:quote)).toList(),
+      )
+    );
+  }
+}
+
+
---
+++ lib/quote.dart
@@ -0,0 +1,11 @@
+class Quote {
+
+  String text;
+  String author;
+  String category;
+  DateTime createdAt;
+
+  Quote({required this.author, required this.text,
+    required this.category, required this.createdAt});
+
+}
